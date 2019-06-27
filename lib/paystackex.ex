8770e2479e7e54aa29befecb55ed0b956c43dea0
atanych <atanych@gmail.com>
2019-06-27 17:53:15 +0300

    Initial commit (mix new)

---
+++ lib/paystackex.ex
@@ -0,0 +1,18 @@
+defmodule Paystackex do
+  @moduledoc """
+  Documentation for Paystackex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Paystackex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/paystackex_test.exs
@@ -0,0 +1,8 @@
+defmodule PaystackexTest do
+  use ExUnit.Case
+  doctest Paystackex
+
+  test "greets the world" do
+    assert Paystackex.hello() == :world
+  end
+end
