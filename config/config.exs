9c645d8f0956aa57ecf43a70c386ac0ade344b5e
atanych <atanych@gmail.com>
2019-07-01 18:41:01 +0300

    Add metadata to config, update ext lib

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,7 @@
 use Mix.Config
 
 config :paystackex, access_token: System.get_env("PAYSTACKEX_ACCESS_TOKEN")
+
+config :logger, :console,
+       format: "$date $time $metadata[$level] $message\n",
+       metadata: [:sdk, :method, :url]
